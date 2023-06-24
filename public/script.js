db9b375ee2e4e3ca048b3a3ea85ff7f8d7080ba1
SeniorVlad1337 <vlad9n455@gmail.com>
2023-06-25 04:30:17 +0600

    asdaz

--- public/script.js
+++ public/script.js
@@ -2,9 +2,19 @@ let modalactive = document.querySelector('.active')
 let openmodal = document.querySelector('.wrapper .containerbutton .btn')
 let modal = document.querySelector('.modal')
 const addtarif = () =>{
+    inputname.value = "";
+    inputlicense.value = "";
+    inputtrack.value = "";
+    inputprice.value = "";
     modal.classList.remove('modal')
     modal.classList.add('active')
+    
 }
+
+function noDigits(event) {
+    if ("1234567890".indexOf(event.key) != -1)
+      event.preventDefault();
+    }
 
 const closemodal = () =>{
     modal.classList.remove('active')
@@ -15,9 +25,37 @@ const closemodal = () =>{
 let inputname = document.querySelector('.modal .containerInput .name')
 let inputlicense = document.querySelector('.modal .containerInput .license')
 let inputtrack = document.querySelector('.modal .containerInput .track')
+let inputprice = document.querySelector('.modal .containerInput .price')
 const addition = () => {
-    let nameinput = inputname.value
-    let licenseinput = inputlicense.value
-    let trackinput = inputtrack.value
-    
+   
 }
+
+
+
+function updatePrice() {
+
+    const projectCount = parseInt(inputlicense.value);
+    const tariffCount = parseInt(inputtrack.value);
+
+
+
+if (!isNaN(projectCount) && !isNaN(tariffCount)) {
+
+    if (projectCount <= 3) {
+      totalPrice = projectCount * (tariffCount * 2000);
+    } else if (projectCount <= 5) {
+      totalPrice = projectCount *  (tariffCount * 1800);
+    } else if (projectCount > 5){
+        totalPrice = projectCount * (tariffCount * 1680);
+    }
+    inputprice.value = totalPrice + "₸";
+} else {
+    inputprice.value = "";
+}
+
+
+
+}
+
+inputlicense.addEventListener('input', updatePrice);
+inputtrack.addEventListener('input', updatePrice);
