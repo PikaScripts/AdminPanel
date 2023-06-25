28397223f9b2c14b088660fd13e612a4e71d0ba3
SeniorVlad1337 <vlad9n455@gmail.com>
2023-06-25 17:15:41 +0600

    z

--- public/product.js
+++ public/product.js
@@ -1,20 +1,18 @@
 let outline = document.querySelector('.find')
 
-let loupe = document.querySelector('.loupe')
-let loupestrok = document.querySelector('.loupestrok')
+let loupe = document.querySelector('.activesvg')
+let loupestrok = document.querySelector('.noactivesvg')
 
 document.addEventListener('click', function(event){
     let clicked = event.target
     outline.style = 'outline: auto'
-    loupe.style = ' stroke: #333333'
-    loupestrok.style = ' stroke: #333333'
-    
+    loupe.style = 'display: block'
+    loupestrok.style = 'display: none'
     if(outline.contains(clicked)){
         return
     }
     outline.style = 'outline: none'
-    loupestrok.style = ' stroke: #D5D5D5'
-    loupe.style = ' stroke: #D5D5D5'
-    
+    loupe.style = 'display: none'
+    loupestrok.style = 'display: block'
 })
 
